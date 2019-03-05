8575e63282e54756783fe546ea437a08054b9f2b
Molly Hopper <mollyhopper95@gmail.com>
2019-03-05 13:58:17 -0500

    Can add recipes to database and all recipes page will display them

--- blue_badge/allrecipes/views.py
+++ blue_badge/allrecipes/views.py
@@ -7,16 +7,17 @@ def all(request):
         my_recipe = models.Recipe.objects.get(id=request.POST['id'])
         my_recipe.save()
 
-    recipes_list = models.Recipe.objects
+    recipes_list = models.Recipe.objects.all()
     context = {
         'recipe_list': recipes_list,
     }
 
     return render(request, 'allrecipes/allrecipes.html', context=context)
 
+
 def add(request):
     if request.method == "POST":
-        my_recipe = models.Item(name=request.POST["name"], ingredients=request.POST["ingredients"], instructions=request.POST["instructions"])
+        my_recipe = models.Recipe(name=request.POST["name"], ingredients=request.POST["ingredients"], instructions=request.POST["instructions"])
         my_recipe.save()
 
         # return redirect('myList')
